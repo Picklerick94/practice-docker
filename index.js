267b369a10b6fb0d78c464d991826014bd82b1c2
jasmine <jastangting@gmail.com>
2021-05-09 18:02:12 +0800

    :added docker files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hi There!!!!! Bearer lizard!!!aa</h2>");
+    res.send("<h1>Snitc</h1>");
 });
 
 const port = process.env.PORT || 3000;
